353d145750f4c123bf0e7e8f6c2c0d4b4b7d3315
DMatthieu <matt.delporte6@gmail.com>
2019-07-26 20:40:07 +0200

    Squelette projet + Sprites + CORRECTION

---
+++ main.lua
@@ -0,0 +1,14 @@
+--Cette ligne permet d'afficher des traces dans la console pendant l'éxécution
+io.stdout:setvbuf('no')
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
